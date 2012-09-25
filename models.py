ee6ae22d56e1227b4ccc7224657e67dc1692eb37
ParkerKuivila@gmail.com <ParkerKuivila@gmail.com>
2012-09-25 00:35:29 -0400

    Add base hello world file, user auth, and framework for backend database

---
+++ models.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import db
+
+class GPS(db.Model):
+    date = db.DateProperty(required=True) # Date of log
+    time = db.TimeProperty(required=True) # Time of log
+    location = db.GeoPtProperty(required=True) # Lat/Long of copter
+    heading = db.IntegerProperty(required=True) # 0-359°, degrees from North
+    rfid_tag = db.StringProperty(required=True) # RFID UID
+    tag_strength = db.FloatProperty(required=True) # Strength of the tag
