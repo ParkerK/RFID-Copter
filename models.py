14fba7df1c9da9091d62c4fea3ce364cf43d6626
ParkerKuivila@gmail.com <ParkerKuivila@gmail.com>
2012-09-26 12:38:53 -0400

    Update some spacing in model

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 from google.appengine.ext import db
 
 class GPS(db.Model):
-	run_name = db.StringProperty()
-    date = db.DateProperty(required=True) # Date of log
-    time = db.TimeProperty(required=True) # Time of log
-    location = db.GeoPtProperty(required=True) # Lat/Long of copter
-    heading = db.IntegerProperty(required=True) # 0-359°, degrees from North
-    altitude = db.FloatProperty(required=True)
-    rfid_tag = db.StringProperty(required=True) # RFID UID
-    tag_strength = db.FloatProperty(required=True) # Strength of the tag
+	run_name     = db.StringProperty() # name of run, if any
+	date         = db.DateProperty(required=True) # Date of log
+	time         = db.TimeProperty(required=True) # Time of log
+	location     = db.GeoPtProperty(required=True) # Lat/Long of copter
+	heading      = db.IntegerProperty(required=True) # 0-359°, degrees from North
+	altitude     = db.FloatProperty(required=True) # altitude, in ft, imperial system ftw
+	rfid_tag     = db.StringProperty(required=True) # RFID UID
+	tag_strength = db.FloatProperty(required=True) # Strength of the tag
