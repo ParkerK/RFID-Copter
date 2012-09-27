5cc4754a21157d6ae74256c99e819cd50e1ade19
ParkerKuivila@gmail.com <ParkerKuivila@gmail.com>
2012-09-27 01:10:25 -0400

    Get write method working

--- copterlogs.py
+++ copterlogs.py
@@ -4,34 +4,38 @@ from google.appengine.ext import db
 
 class CopterLog(db.Model):
     run_name     = db.StringProperty() # name of run, if any
-    date         = db.DateProperty(required=True) # Date of log
-    time         = db.TimeProperty(required=True) # Time of log
-    location     = db.GeoPtProperty(required=True) # Lat/Long of copter
-    heading      = db.IntegerProperty(required=True) # 0-359, degrees from North
-    altitude     = db.FloatProperty(required=True) # altitude, in ft, imperial system ftw
+    date         = db.DateProperty(auto_now_add=True) # Date of log
+    time         = db.TimeProperty(auto_now_add=True) # Time of log
+    location     = db.GeoPtProperty() # Lat/Long of copter
+    heading      = db.IntegerProperty() # 0-359, degrees from North
+    altitude     = db.FloatProperty() # altitude, in ft, imperial system ftw
     rfid_tag     = db.StringProperty() # RFID UID
     tag_strength = db.FloatProperty() # Strength of the tag
 
+
+def copterlog_key(copterlog_name=None):
+    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+    return db.Key.from_path('CopterLog', copterlog_name or 'default_copterlog')
+
 class Write(webapp2.RequestHandler):
     def get(self):
-        copterlog = new CopterLog()
+        copterlog = CopterLog(parent=copterlog_key())
         copterlog.run_name     = cgi.escape(self.request.get('run_name'))
-        copterlog.date         = cgi.escape(self.request.get('date'))
-        copterlog.time         = cgi.escape(self.request.get('time'))
+        # copterlog.date         = cgi.escape(self.request.get('date'))
+        # copterlog.time         = cgi.escape(self.request.get('time'))
         copterlog.location     = cgi.escape(self.request.get('location'))
-        copterlog.heading      = cgi.escape(self.request.get('heading'))
-        copterlog.altitude     = cgi.escape(self.request.get('altitude'))
+        copterlog.heading      = int(cgi.escape(self.request.get('heading')))
+        copterlog.altitude     = float(cgi.escape(self.request.get('altitude')))
         copterlog.rfid_tag     = cgi.escape(self.request.get('rfid_tag'))
-        copterlog.tag_strength = cgi.escape(self.request.get('tag_strength'))
+        copterlog.tag_strength = float(cgi.escape(self.request.get('tag_strength')))
 
-        user = users.get_current_user()
+        copterlog.put()
+        # user = users.get_current_user()
 
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + run_name)
-            copterlog.put()
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        # if user:
+        #     copterlog.put()
+        # else:
+        #     self.redirect(users.create_login_url(self.request.uri))
 
 class Read(webapp2.RequestHandler):
     def get(self):
--- main.py
+++ main.py
@@ -24,18 +24,28 @@
 #     ('/', MainHandler)
 # ], debug=True)
 import webapp2
+import jinja2
+import os
+import copterlogs
 
+jinja_environment = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
+	def get(self):
+		user = users.get_current_user()
+		logs = copterlogs.CopterLog.all()
 
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+		if user:
+			template_values = {
+				'console_logs': logs,
+			}
+
+			template = jinja_environment.get_template('index.html')
+			self.response.out.write(template.render(template_values))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+							  debug=True)
