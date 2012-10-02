31652fedbf6255809fc53be49bb5d8e458c76b33
ParkerKuivila@gmail.com <ParkerKuivila@gmail.com>
2012-10-02 10:43:19 -0400

    Update API for /read/, will now return JSON of data from a given run_name

--- copterlogs.py
+++ copterlogs.py
@@ -1,6 +1,7 @@
 import webapp2, cgi
 from google.appengine.api import users
 from google.appengine.ext import db
+import simplejson
 
 class CopterLog(db.Model):
     run_name     = db.StringProperty() # name of run, if any
@@ -11,7 +12,6 @@ class CopterLog(db.Model):
     altitude     = db.FloatProperty() # altitude, in ft, imperial system ftw
     rfid_tag     = db.StringProperty() # RFID UID
     tag_strength = db.FloatProperty() # Strength of the tag
-
 
 def copterlog_key(copterlog_name=None):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
@@ -28,7 +28,6 @@ class Write(webapp2.RequestHandler):
         copterlog.altitude     = float(cgi.escape(self.request.get('altitude')))
         copterlog.rfid_tag     = cgi.escape(self.request.get('rfid_tag'))
         copterlog.tag_strength = float(cgi.escape(self.request.get('tag_strength')))
-
         copterlog.put()
         # user = users.get_current_user()
 
@@ -37,17 +36,26 @@ class Write(webapp2.RequestHandler):
         # else:
         #     self.redirect(users.create_login_url(self.request.uri))
 
+def gql_json_parser(query_obj):
+    result = []
+    for entry in query_obj:
+        result.append(dict([(p, unicode(getattr(entry, p))) for p in entry.properties()]))
+    return result
+
 class Read(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
+        run_name = cgi.escape(self.request.get('run_name'))
+        logs = CopterLog.all()
 
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        if run_name:
+            logs.filter("run_name =", run_name)
+        
+        json_query_data =  gql_json_parser(logs)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(simplejson.dumps(json_query_data))
+
 
 app = webapp2.WSGIApplication([
-                ('/write/', Write),
+                ('/write/   ', Write),
                 ('/read/', Read)
                 ], debug=True)
