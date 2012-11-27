4e84c2b479e3732fb1e0c6aacb00b06877c7593b
John O'Hollaren <johnpohollaren@gmail.com>
2012-11-27 02:21:54 -0500

    updated matlab and python scripts

---
+++ hardware/RFID_reader/logs/nov20/modified/clean_input.py
@@ -0,0 +1,19 @@
+import sys, string, re
+
+original_filename = sys.argv[1]
+no_ext_file_name = original_filename.replace(".txt","")
+cleaned_up_filename = no_ext_file_name + "_clean.txt"
+
+f = open(cleaned_up_filename, 'wb')
+
+g = open("rejects.txt", 'wb')
+
+for line in open(original_filename, 'r'):
+	line_new = line.replace("[]","0.0")
+	s = [m.start() for m in re.finditer(' ', line_new)]
+	
+	if ( ((len(line_new) == 24) and (s[0] == 2) and (s[1] == 12)) or ((len(line_new) == 25) and (s[0] == 3) and (s[1] == 13)) ):
+		f.write(line_new)
+	else:
+		g.write(line_new)
+
